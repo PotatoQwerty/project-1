6cdfc290d972c6a1c050dca8877f92d01288a549
PotatoQwerty <alhallak5islam@hotmail.fr>
2025-04-13 22:48:19 +0100

    refactor: replace text link with logo image in Layout component for improved branding

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import Header from "../../components/Header";
 import { AuthProvider } from "../../context/AuthContext";
+import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Shopie | Create Account or Sign In",
@@ -15,12 +16,11 @@ export default function Layout({ children }) {
       <body className={inter.className}>
         <AuthProvider>
           <main>
-            <Link
-              className="text-3xl font-bold text-primary m-3 items-center flex justify-center"
-              href="/"
-            >
-              Shopie.
-            </Link>
+            <header className="flex flex-col items-center justify-center bg-white shadow-md p-4 h-fit">
+              <Link href="/">
+                <Image src="/logo.png" alt="logo" width={100} height={100} />
+              </Link>
+            </header>
             {children}
           </main>
         </AuthProvider>
